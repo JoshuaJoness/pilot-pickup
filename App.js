2a6dc3f16a7a40316ddb73477bb759c0f7f4268b
Joshua Jones <joshuajones@trentu.ca>
2020-01-25 11:43:59 -0500

    Added Main.js (working)

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/main'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>All is well...</Text>
+			<Main/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
